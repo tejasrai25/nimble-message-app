4a59b93706a98093ae1c68defbcc377046d7b0a6
Tejas Rai <tejasrai25@gmail.com>
2021-12-12 23:34:57 -0500

    #7 chat message component

--- frontend/src/components/ChatMessage.tsx
+++ frontend/src/components/ChatMessage.tsx
@@ -1,24 +1,38 @@
 import * as React from 'react';
-import { Paper, Stack } from '@mui/material';
+import { Paper, Typography, Grid } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const Chat = styled(Paper)(({ theme }) => ({
+const Chat = (sentMessage: boolean) => (styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
-    textAlign: 'left',
     color: theme.palette.text.primary,
-    backgroundColor: theme.palette.secondary.light,
-    height: 60,
-    lineHeight: '60px',
+    backgroundColor: sentMessage ? theme.palette.primary.light : theme.palette.grey[200],
+})));
+
+const PaddedText = styled(Typography)(() => ({
+    padding: '5px'
 }));
 
 export default ({ message, sender, sentTime }: { message: string, sender: string, sentTime: Date }) => {
+    const ColoredChat = Chat(sender === 'me')
     return (
-        <Stack>
-            <div>{sender}</div>
-            <Chat elevation={3}>
-                {message}
-            </Chat>
-            <div>{sentTime}</div>
-        </Stack>
+        <Grid container justifyContent={sender === 'me' ? 'flex-end' : 'flex-start'}>
+            <Grid item xs={5} >
+                <ColoredChat elevation={3}>
+                    <Grid container justifyContent={'flex-start'}>
+                        <Grid item xs={6} >
+                            <PaddedText variant='caption' >{sender}</PaddedText>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <PaddedText variant='body2'>{message}</PaddedText>
+                        </Grid>
+                        <Grid container justifyContent={'flex-end'}>
+                            <Grid container xs={6} justifyContent={'flex-end'}>
+                                <PaddedText variant='caption' align='right'>{`${sentTime.toLocaleDateString()} ${sentTime.toLocaleTimeString()}`}</PaddedText>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </ColoredChat >
+            </Grid >
+        </Grid >
     )
 }
