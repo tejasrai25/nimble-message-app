0883413fa5cf40b5099bdee2383fde128b45a688
Tejas Rai <tejasrai25@gmail.com>
2021-12-13 02:24:28 -0500

    #7 background for chat pane

--- frontend/src/layouts/ChatPane.tsx
+++ frontend/src/layouts/ChatPane.tsx
@@ -51,7 +51,7 @@ export default () => {
     }, [messages]);
     return (
         <Grid container>
-            <Stack spacing={2} style={{ height: '90vh', overflow: 'auto', padding: '10px' }}>
+            <Stack spacing={2} sx={{ height: '90vh', overflow: 'auto', padding: '10px', backgroundColor: 'grey.100' }}>
                 {messages.map((message) => {
                     const { id, ...messageProps } = message
                     return <ChatMessage key={`chat-${message.id}`} {...messageProps} />
