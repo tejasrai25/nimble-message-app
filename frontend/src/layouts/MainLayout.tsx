56bb1e34c479f9dd84822d7061cc0f245e2ece5d
Tejas Rai <tejasrai25@gmail.com>
2021-12-13 03:19:33 -0500

    #13 Top bar added

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -2,16 +2,20 @@ import * as React from 'react';
 import { Grid } from '@mui/material';
 import ContactList from './ContactList';
 import ChatPane from './ChatPane';
+import TopBar from './TopBar';
 
 export default () => {
     return (
-        <Grid container spacing={0}>
-            <Grid item xs={4}>
-                <ContactList />
+        <>
+            <TopBar />
+            <Grid container spacing={0}>
+                <Grid item xs={4}>
+                    <ContactList />
+                </Grid>
+                <Grid item xs={8}>
+                    <ChatPane />
+                </Grid>
             </Grid>
-            <Grid item xs={8}>
-                <ChatPane />
-            </Grid>
-        </Grid>
+        </>
     );
 }
