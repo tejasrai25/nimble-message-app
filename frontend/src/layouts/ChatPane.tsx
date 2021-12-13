a0ecad799c0a8a4da2e5e738b8968eb08a1c93f7
Tejas Rai <tejasrai25@gmail.com>
2021-12-13 01:27:07 -0500

    #7 updated type

--- frontend/src/layouts/ChatPane.tsx
+++ frontend/src/layouts/ChatPane.tsx
@@ -2,50 +2,46 @@ import * as React from 'react';
 import { Grid, Stack } from '@mui/material';
 import ChatMessage from '../components/ChatMessage';
 import ChatBox from '../components/ChatBox';
+import { Message } from '../models';
 
 export default () => {
-    const messages: {
-        id: number;
-        message: string;
-        sender: string;
-        sentTime: Date;
-    }[] = [
-            {
-                id: 1,
-                message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                sender: 'Sabreesh',
-                sentTime: new Date(),
-            },
-            {
-                id: 2,
-                message: 'Lorem ipsum',
-                sender: 'me',
-                sentTime: new Date(),
-            },
-            {
-                id: 3,
-                message: 'Lorem ipsum sit',
-                sender: 'Sabreesh',
-                sentTime: new Date(),
-            },
-            {
-                id: 4,
-                message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                sender: 'me',
-                sentTime: new Date(),
-            }, {
-                id: 5,
-                message: 'Lorem ipsum sit',
-                sender: 'Sabreesh',
-                sentTime: new Date(),
-            },
-            {
-                id: 6,
-                message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                sender: 'me',
-                sentTime: new Date(),
-            },
-        ]
+    const messages: Message[] = [
+        {
+            id: 1,
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            sender: 'Sabreesh',
+            sentTime: new Date(),
+        },
+        {
+            id: 2,
+            message: 'Lorem ipsum',
+            sender: 'me',
+            sentTime: new Date(),
+        },
+        {
+            id: 3,
+            message: 'Lorem ipsum sit',
+            sender: 'Sabreesh',
+            sentTime: new Date(),
+        },
+        {
+            id: 4,
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            sender: 'me',
+            sentTime: new Date(),
+        }, {
+            id: 5,
+            message: 'Lorem ipsum sit',
+            sender: 'Sabreesh',
+            sentTime: new Date(),
+        },
+        {
+            id: 6,
+            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            sender: 'me',
+            sentTime: new Date(),
+        },
+    ]
 
     const scrollRef = React.useRef<HTMLDivElement>(null);
     React.useEffect(() => {
