d25ce664c863013ad9090d9579f0ab9185d7ce01
Tejas Rai <tejasrai25@gmail.com>
2021-12-14 12:22:44 -0500

    #23 register functionality

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,10 +1,38 @@
-import { Button, Grid, Stack, TextField } from '@mui/material';
+import { Alert, Button, Grid, Snackbar, Stack, TextField } from '@mui/material';
 import * as React from 'react';
 
 
 export default () => {
     const [register, setRegister] = React.useState(false);
-    const registerUser = () => { setRegister(false) }
+    const [fullName, setFullName] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [showSnackbar, setShowSnackbar] = React.useState(false);
+    const [error, setError] = React.useState('');
+
+    const exitRegister = ({ snackbar }: { snackbar: boolean }) => {
+        if (snackbar) {
+            setShowSnackbar(true)
+        }
+        setRegister(false)
+        setFullName('')
+    }
+
+    const registerUser = () => {
+        fetch('http://localhost:5000/api/users', {
+            method: 'post',
+            body: JSON.stringify({ name: fullName.trim(), username: username.trim(), password: password.trim() })
+        }
+        ).then(r => r.json())
+            .then(result => {
+                if (result.username) {
+                    exitRegister({ snackbar: true })
+                }
+                else if (result.error) {
+                    setError(result.message)
+                }
+            })
+    }
     return (
         <Grid
             container
@@ -27,23 +55,56 @@ export default () => {
                     {register && <TextField
                         label="Name"
                         variant="filled"
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                        error={Boolean(error)}
                     />}
                     <TextField
                         label="Username"
                         variant="filled"
+                        value={username}
+                        onChange={(event) => { setUsername(event.target.value) }}
+                        error={Boolean(error)}
                     />
                     <TextField
                         label="Password"
                         variant="filled"
                         type={'password'}
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                        error={Boolean(error)}
+                        helperText={error ? error : ''}
                     />
-                    {!register && <Button variant='contained'>Login</Button>}
+                    {!register && <Button
+                        variant='contained'
+                        onClick={() => {
+                            setError('')
+                        }}
+                    >
+                        Login
+                    </Button>}
                     <Button
                         variant={register ? 'contained' : undefined}
-                        onClick={() => { register ? registerUser() : setRegister(true) }}
+                        onClick={() => {
+                            setError('')
+                            register ? registerUser() : setRegister(true)
+                        }}
                     >
                         Register
                     </Button>
+                    {register && <Button
+                        onClick={() => {
+                            setError('')
+                            exitRegister({ snackbar: false })
+                        }}
+                    >
+                        Back to Login
+                    </Button>}
+                    <Snackbar open={showSnackbar} autoHideDuration={6000} onClose={() => { setShowSnackbar(false) }}>
+                        <Alert severity="success" sx={{ width: '100%' }}>
+                            User Registered Successfully!
+                        </Alert>
+                    </Snackbar>
                 </Stack>
             </Grid>
         </Grid >
