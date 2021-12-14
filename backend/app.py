1b8fb975e6671df0cdc667019445c07491e49183
Tejas Rai <tejasrai25@gmail.com>
2021-12-14 05:04:06 -0500

    #22 login completed

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,58 @@
-from flask import Flask
+from flask import Flask, request
+import flask_praetorian
+from peewee import IntegrityError
+from schema import db, User
+
 app = Flask(__name__)
+
+# Add JWT Token plugin
+guard = flask_praetorian.Praetorian()
+
+app.config['SECRET_KEY'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'
+app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
+app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
+
+guard.init_app(app, User)
+
+
+@app.before_request
+def before_request():
+    db.connect()
+
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    Logs a user in by parsing a POST request containing user credentials and
+    issuing a JWT token.
+    """
+    req = request.get_json(force=True)
+    username = req.get("username", None)
+    password = req.get("password", None)
+    user = guard.authenticate(username, password)
+    ret = {"access_token": guard.encode_jwt_token(user)}
+    return ret, 200
+
+
+@app.route("/api/users", methods=["POST"])
+def register():
+    req = request.get_json(force=True)
+    username = req.get("username")
+    password = req.get("password")
+    name = req.get("name")
+    try:
+        user = User.create(username=username,
+                           password=guard.hash_password(password),
+                           name=name)
+        return {'username': user.username, 'name': user.name}, 201
+    except IntegrityError:
+        return {'error': 'IntegrityError', 'message': 'The username is already in use'}, 400
 
 
 @app.route('/')
--- backend/schema.py
+++ backend/schema.py
@@ -25,6 +25,56 @@ class BaseModel(Model):
 class User(BaseModel):
     username = CharField(unique=True)
     name = CharField()
+    password = CharField()
+
+    @property
+    def identity(self):
+        """
+        *Required Attribute or Property*
+
+        flask-praetorian requires that the user class has an ``identity`` instance
+        attribute or property that provides the unique id of the user instance
+        """
+        return self.id
+
+    @property
+    def rolenames(self):
+        """
+        *Required Attribute or Property*
+
+        flask-praetorian requires that the user class has a ``rolenames`` instance
+        attribute or property that provides a list of strings that describe the roles
+        attached to the user instance
+        """
+        return []
+
+    @staticmethod
+    def lookup(username):
+        """
+        *Required Method*
+
+        flask-praetorian requires that the user class implements a ``lookup()``
+        class method that takes a single ``username`` argument and returns a user
+        instance if there is one that matches or ``None`` if there is not.
+        """
+        return User.get_or_none(User.username == username)
+
+    @classmethod
+    def identify(cls, id):
+        """
+        *Required Method*
+
+        flask-praetorian requires that the user class implements an ``identify()``
+        class method that takes a single ``id`` argument and returns user instance if
+        there is one that matches or ``None`` if there is not.
+        """
+        try:
+            return User.get_by_id(id)
+        except DoesNotExist:
+            return None
+
+    def is_valid(self):
+        return True
 
 
 class Message(BaseModel):
@@ -36,4 +86,5 @@ class Message(BaseModel):
 
 if __name__ == '__main__':
     db.connect()
+    db.drop_tables([User, Message])
     db.create_tables([User, Message])
