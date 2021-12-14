843732901a3ec07878c4483dd6b38858d9e5ec17
Tejas Rai <tejasrai25@gmail.com>
2021-12-14 15:05:28 -0500

    #25 strorage fix

--- frontend/src/auth/index.ts
+++ frontend/src/auth/index.ts
@@ -4,5 +4,4 @@ import { AuthenticatedUser } from '../models';
 export const { useAuth, authFetch, login, logout, getSessionState } =
     createAuthProvider<AuthenticatedUser>({
         getAccessToken: (session) => (session['access_token']),
-        storageKey: 'access_token',
     });
