a832bbcf751be78a3330bea984d1457d63d8095a
Tejas Rai <tejasrai25@gmail.com>
2021-12-14 06:53:44 -0500

    #22 implemented message create and fetch

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import flask_praetorian
 from peewee import IntegrityError
-from schema import db, User
+from schema import db, User, Message
 
 app = Flask(__name__)
 
@@ -62,7 +62,7 @@ def register():
 @flask_praetorian.auth_required
 def fetch_users():
     '''
-    Fetch all users
+    Fetch all users except the current user
     '''
     current_user = flask_praetorian.current_user().username
     users = [{'id': user.id, 'username': user.username, 'name': user.name}
@@ -70,6 +70,62 @@ def fetch_users():
     return jsonify(users), 200
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+@app.route('/api/messages', methods=['POST'])
+@flask_praetorian.auth_required
+def store_message():
+    '''
+    Store message between current user and receiver
+    '''
+    req = request.get_json(force=True)
+    message = req.get('message')
+    receiver = User.lookup(req.get('receiver'))
+    current_user = flask_praetorian.current_user()
+    if not message or not receiver:
+        return {'error': 'ValueError', 'message': 'body must contain message and a valid username as receiver'}, 400
+    if receiver.username == current_user.username:
+        return {'error': 'ValueError', 'message': 'user cannot send message to themselves'}, 400
+
+    message = Message.create(
+        message=message,
+        sender=current_user,
+        receiver=receiver,
+    )
+
+    return {
+        'id': message.id,
+        'message': message.message,
+        'sender': message.sender.username,
+        'receiver': message.receiver.username,
+        'sentTime': message.sent_time.isoformat()
+    }, 201
+
+
+@app.route('/api/messages', methods=['GET'])
+@flask_praetorian.auth_required
+def fetch_messages():
+    '''
+    Fetch all messages between current user and another specified user
+    '''
+    current_user = flask_praetorian.current_user()
+    other_user = User.lookup(request.args.get('user'))
+
+    if not other_user:
+        return {'error': 'ValueError', 'message': 'user parameter is required and must be a valid username'}, 400
+    Sender = User.alias()
+    Receiver = User.alias()
+    messages = [
+        {
+            'id': message.id,
+            'message': message.message,
+            'sender': message.sender.username,
+            'receiver': message.receiver.username,
+            'sentTime': message.sent_time.isoformat()
+        } for message in Message.select()
+        .join(Sender, on=Message.sender)
+        .where(Sender.username.in_([current_user.username, other_user.username]))
+        .switch(Message)
+        .join(Receiver, on=Message.receiver)
+        .where(Receiver.username.in_([current_user.username, other_user.username]))
+        .order_by(Message.sent_time.asc())
+    ]
+    return jsonify(messages), 200
